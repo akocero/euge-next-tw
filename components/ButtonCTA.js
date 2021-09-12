02b7ecfdf6990742f5d9e3e77b666a84a2bb4b74
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-09-12 14:42:35 +0800

    fix: button cta UI in light mode

--- components/ButtonCTA.js
+++ components/ButtonCTA.js
@@ -1,6 +1,6 @@
 const ButtonCTA = () => {
 	return (
-		<button className="text-lg px-6 dark:border-yellow-600 border-yellow-500 font-semibold dark:font-normal border-2 py-2 rounded-xl dark:text-gray-100 text-gray-900 hover:shadow-xl mr-4 hover:-translate-y-1 default-transition">
+		<button className="text-lg px-6 border-0 text-white bg-yellow-500 dark:border-yellow-600 border-yellow-500 font-normal dark:border-2 py-2 rounded-xl dark:text-gray-100 dark:bg-transparent  hover:shadow-xl mr-4 hover:-translate-y-1 default-transition">
 			let's talk
 		</button>
 	);
