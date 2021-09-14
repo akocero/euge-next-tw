04be60a1c36f0a0f1d911d5b38ea7f10a4b49ec1
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-09-14 20:24:29 +0800

    update: social links UI in Light Mode

--- pages/index.js
+++ pages/index.js
@@ -68,17 +68,17 @@ export default function Home() {
 				isScrollingDown={scrollState}
 			/>
 			<ul className="links flex flex-col items-start fixed bottom-4 left-10 text-xl text-gray-800 dark:text-gray-200">
-				<li className="mb-4 dark:bg-primary bg-gray-200 p-3 rounded-xl">
+				<li className="mb-4 dark:bg-primary bg-gray-100 p-3 rounded-xl border-gray-300 border dark:border-gray-800">
 					<FiGithub />
 				</li>
 
-				<li className="mb-4 dark:bg-primary bg-gray-200 p-3 rounded-xl">
+				<li className="mb-4 dark:bg-primary bg-gray-100 p-3 rounded-xl border-gray-300 border dark:border-gray-800">
 					<FiLinkedin />
 				</li>
-				<li className="mb-4 dark:bg-primary bg-gray-200 p-3 rounded-xl">
+				<li className="mb-4 dark:bg-primary bg-gray-100 p-3 rounded-xl border-gray-300 border dark:border-gray-800">
 					<FiTwitter />
 				</li>
-				<li className="mb-4 dark:bg-primary bg-gray-200 p-3 rounded-xl">
+				<li className="mb-4 dark:bg-primary bg-gray-100 p-3 rounded-xl border-gray-300 border dark:border-gray-800">
 					<FiFacebook />
 				</li>
 				{/* <li className="border h-36 ml-5 border-gray-800"></li> */}
